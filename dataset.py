b6182badbc254e7dc3072dcb9c50eb6a7da33e7a
Sunghoon Baek <seanbb93@gmail.com>
2021-08-03 17:18:33 +0900

    bert modulization

--- dataset.py
+++ dataset.py
@@ -71,49 +71,51 @@ class COCO_Dataset(Dataset):
     #     return caption_text
 
 
-_cls_token = '[CLS]'
-_sep_token = '[SEP]'
+class BertEmbeddings():
+    def __init__(self):
+        self._cls_token = '[CLS]'
+        self._sep_token = '[SEP]'
+        self._tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self._bert_layer = BertModel.from_pretrained('bert-base-uncased')
+        # _bert_layer = torch.nn.DataParallel(_bert_layer)
+        # _bert_layer = _bert_layer.cuda()
+        self._bert_layer.eval()
 
-_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-_bert_layer = BertModel.from_pretrained('bert-base-uncased')
-# _bert_layer = torch.nn.DataParallel(_bert_layer)
-# _bert_layer = _bert_layer.cuda()
-_bert_layer.eval()
+    def get_bert_for_caption(self, captions: List[str], max_text_length: int = 17):
+        """Returns BERT pooled and sequence outputs for a given list of captions."""
+        all_tokens = []
+        all_input_mask = []
+        for text in captions:
+            truncated_tokens = self._tokenizer.tokenize(text)[:max_text_length - 2]
+            tokens = [self._cls_token] + truncated_tokens + [self._sep_token]
+            tokens = self._tokenizer.convert_tokens_to_ids(tokens)
+            num_padding = max_text_length - len(tokens)
+            input_mask = [1] * len(tokens)
+            tokens = tokens + [0] * num_padding
+            input_mask = input_mask + [0] * num_padding
+            all_tokens.append(np.asarray(tokens, np.int32))
+            all_input_mask.append(np.asarray(input_mask, np.int32))
 
+        ids = torch.tensor(all_tokens)
+        input_mask = torch.tensor(all_input_mask, )
+        segment_ids = torch.zeros_like(ids)
 
-def get_bert_for_caption(captions: List[str], max_text_length: int = 17):
-    """Returns BERT pooled and sequence outputs for a given list of captions."""
-    all_tokens = []
-    all_input_mask = []
-    for text in captions:
-        truncated_tokens = _tokenizer.tokenize(text)[:max_text_length - 2]
-        tokens = [_cls_token] + truncated_tokens + [_sep_token]
-        tokens = _tokenizer.convert_tokens_to_ids(tokens)
-        num_padding = max_text_length - len(tokens)
-        input_mask = [1] * len(tokens)
-        tokens = tokens + [0] * num_padding
-        input_mask = input_mask + [0] * num_padding
-        all_tokens.append(np.asarray(tokens, np.int32))
-        all_input_mask.append(np.asarray(input_mask, np.int32))
+        # print(ids)
+        # print(input_mask)
+        # print(segment_ids)
 
-    ids = torch.tensor(all_tokens)
-    input_mask = torch.tensor(all_input_mask, )
-    segment_ids = torch.zeros_like(ids)
-
-    # print(ids)
-    # print(input_mask)
-    # print(segment_ids)
-
-    with torch.no_grad():
-        token_embedding = _bert_layer(ids, attention_mask=input_mask, token_type_ids=segment_ids)
-        token_embedding = token_embedding[0]
-        max_len = torch.sum(input_mask, dim=1)
-        sent_embedding = torch.sum(token_embedding, dim=1) / max_len[:, None]
-    return token_embedding, sent_embedding, max_len
+        with torch.no_grad():
+            token_embedding = self._bert_layer(ids, attention_mask=input_mask, token_type_ids=segment_ids)
+            token_embedding = token_embedding[0]
+            max_len = torch.sum(input_mask, dim=1)
+            sent_embedding = torch.sum(token_embedding, dim=1) / max_len[:, None]
+        return token_embedding, sent_embedding, max_len
 
 
 set_list = ['train2014', 'val2014']
 start_time = datetime.now()
+bert = BertEmbeddings()
+
 for set_name in set_list:
     print('\n------------------------------------------------------')
     test_class = COCO_Dataset(set_name=set_name)
@@ -127,11 +129,11 @@ for set_name in set_list:
     for idx, (images, captions) in enumerate(test_loader):
         # print(images)
         # print(captions)
-        t1, t2, t3 = get_bert_for_caption(captions)
+        t1, t2, t3 = bert.get_bert_for_caption(captions)
         print(f'\r{(idx + 1) * 64}/{test_class.__len__()}\t{round(((idx + 1) * 64 / test_class.__len__()) * 100, 1)}%',
               end='')
-        # if idx == 0:
-        #     break
+        # if idx == 10:
+        #      break
 
 end_time = datetime.now()
 
